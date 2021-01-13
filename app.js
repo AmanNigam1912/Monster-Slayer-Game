90a36a0d0c7dafb352602b29727c34dade12d0a1
Aman Nigam <nigam.ama@husky.neu.edu>
2021-01-12 19:55:39 -0500

    Monster Slayer Game

---
+++ app.js
@@ -0,0 +1,114 @@
+function getRandomValue(min, max) {
+  return Math.floor(Math.random() * (max - min)) + min;
+}
+
+// Create a Vue App
+const app = Vue.createApp({
+  data() {
+    return {
+      playerHealth: 100,
+      monsterHealth: 100,
+      currentRound: 0,
+      winner: null,
+      logMessages: [],
+    };
+  },
+  computed: {
+    monsterBarStyles() {
+      if (this.monsterHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.monsterHealth + "%" };
+    },
+    playerBarStyles() {
+      if (this.playerHealth < 0) {
+        return { width: "0%" };
+      }
+      return { width: this.playerHealth + "%" };
+    },
+    // Option to launch a special attack after every 3 rounds
+    mayUseSpecialAttack() {
+      return this.currentRound % 3 !== 0;
+    },
+  },
+  watch: {
+    playerHealth(value) {
+      if (value <= 0 && this.monsterHealth <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Player lost
+        this.winner = "monster";
+      }
+    },
+    monsterHealth(value) {
+      if (value <= 0 && this.playerHealth <= 0) {
+        // A draw
+        this.winner = "draw";
+      } else if (value <= 0) {
+        // Monster lost
+        this.winner = "player";
+      }
+    },
+  },
+  methods: {
+    // Method to start a new game
+    startGame() {
+      this.playerHealth = 100;
+      this.monsterHealth = 100;
+      this.currentRound = 0;
+      this.winner = null;
+      this.logMessages = [];
+    },
+    // Method when monster is attacked by player
+    attackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(5, 12);
+      this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "attack", attackValue);
+      this.attackPlayer();
+    },
+    // Method when player is attacked by monster
+    attackPlayer() {
+      // Monster hit the player harder
+      const attackValue = getRandomValue(8, 15);
+      this.playerHealth -= attackValue;
+      this.addLogMessage("monster", "attack", attackValue);
+    },
+    // Method when monster is attacked by player with more damage
+    specialAttackMonster() {
+      this.currentRound++;
+      const attackValue = getRandomValue(10, 25);
+      this.monsterHealth -= attackValue;
+      this.addLogMessage("player", "attack", attackValue);
+      this.attackPlayer();
+    },
+    // Heal the player
+    healPlayer() {
+      this.currentRound++;
+      const healValue = getRandomValue(8, 20);
+      if (this.playerHealth + healValue > 100) {
+        this.playerHealth = 100;
+      } else {
+        this.playerHealth += healValue;
+      }
+      this.addLogMessage("player", "heal", healValue);
+      // Monster will attack the player after healing
+      this.attackPlayer();
+    },
+    // If player surrenders then monster wins
+    surrender() {
+      this.winner = "monster";
+    },
+    // Display log messages for every action type of player
+    addLogMessage(who, what, value) {
+      this.logMessages.unshift({
+        actionBy: who,
+        actionType: what,
+        actionValue: value,
+      });
+    },
+  },
+});
+
+app.mount("#game");
